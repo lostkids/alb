32a11adcd2eebd5807ba082df916c1c7b2f8edbe
fingadumbledore <fingadumbledore@gmail.com>
2022-06-02 16:54:42 +0200

    modified:   setup.sh

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+echo --------------------------[Checking System]----------------------
+platform='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   platform='freebsd'
+fi
+echo $platform
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt-get
+osInfo[/etc/alpine-release]=apk
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        echo Package manager: ${osInfo[$f]}
+    fi
+done
+
